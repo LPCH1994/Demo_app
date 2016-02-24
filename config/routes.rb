bb866f27f0b5203d73a50def94c20c1b42ddf914
LPCH1994 <hsblh32@gmail.com>
2016-02-24 15:33:55 +0700

    Finish log in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
   root 'pages#home'
   get 'signup' => 'users#new'
   resources :users
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
 
 
