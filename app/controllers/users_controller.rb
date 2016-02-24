0bbff207d64a6527c8a10e5834daec421e01b3d8
LPCH1994 <hsblh32@gmail.com>
2016-02-24 15:03:02 +0700

    add something

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      flash[:success] = "Welcome to Blog service!"
       redirect_to @user
     else
       render 'new'
