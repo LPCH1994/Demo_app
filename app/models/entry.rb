c520395fa78fbece2d66dd0febb46e4b04f50ba8
LPCH1994 <hsblh32@gmail.com>
2016-03-01 10:35:49 +0700

    finish entry

---
+++ app/models/entry.rb
@@ -0,0 +1,8 @@
+class Entry < ActiveRecord::Base
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :title, presence: true,length: {maximum: 30}
+  validates :body,presence: true,length: {maximum: 140}
+
+end
