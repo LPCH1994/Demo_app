1cc7db52e1fe2593a23daf74254a206d160ca3aa
LPCH1994 <hsblh32@gmail.com>
2016-03-01 08:21:36 +0700

    mini commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Administrator",
+             email: "admin@gmail.com",
+             password:              "admin",
+             password_confirmation: "admin",
              admin: true)
 
 99.times do |n|
